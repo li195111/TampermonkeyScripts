8e4b81e539dde4243f65f84f511a3f21b23194a1
li195111 <green07111@gmail.com>
2023-02-03 14:25:26 +0800

    update python venv

--- run_eyny.bat
+++ run_eyny.bat
@@ -1,2 +1,2 @@
 set conda='C:\Users\a0983\miniconda3\shell\condabin\conda-hook.ps1'
-wt powershell -noexit -command "& {& %conda%\;conda activate scraper\;python eyny_url_download.py}"
+wt powershell -noexit -command "& {& %conda%\;conda activate scrapper39\;python eyny_url_download.py}"

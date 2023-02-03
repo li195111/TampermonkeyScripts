4642444e5e35e87b4c98706c87c1cf0aa627cbb9
li195111 <green07111@gmail.com>
2023-02-03 15:43:41 +0800

    update overview

--- overview_files.py
+++ overview_files.py
@@ -11,7 +11,6 @@ if __name__ == "__main__":
 
   # 1 MB
   min_size = 1024**2
-  print(f'Min Size: {min_size}')
   for dir_path in dir_paths:
     for vid in glob.glob(f'{dir_path}/*/*.mp4', recursive=True):
       vid_dir_path = os.path.dirname(vid)
@@ -19,12 +18,11 @@ if __name__ == "__main__":
       code = re.findall(
           r'[a-zA-Z]?[a-zA-Z]?[a-zA-Z]?[a-zA-Z][a-zA-Z]-[0-9][0-9][0-9][0-9]?',
           vid_name)
-      fc_series = re.findall(r'FC2*', vid_name)
-      if len(code) > 0 or len(fc_series) > 0:
+      fcppv_code = re.findall(r'FC2[\-]?PPV[\-]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?', vid_name)
+      if len(code) > 0 or len(fcppv_code) > 0:
         if (os.path.exists(vid)):
           if os.path.getsize(vid) < min_size:
-            print(code, vid_dir_path)
-            # shutil.rmtree(vid_dir_path)
-  # vid_dir_names = os.listdir(dir_path)
-  # for vid_dir_name in vid_dir_names:
-  #   vid_dir_path = os.path.join(dir_path,vid_dir_name)
+            if len(fcppv_code) == 0:
+              print(code, vid_dir_path)
+            else:
+              print(fcppv_code, vid_dir_path)

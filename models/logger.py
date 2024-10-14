1f7f1579a3341d9e0106fb1d28ab8689c2c04277
YueLi <green07111@gmail.com>
2024-10-14 16:18:37 +0800

    Refactor pyproject.toml to add aiofiles dependency Refactor logger.py to simplify logging configuration Refactor send_video_to_tg_channel.py use aiofiles

--- models/logger.py
+++ models/logger.py
@@ -7,33 +7,6 @@ import sys
 from logging.handlers import TimedRotatingFileHandler
 from pathlib import Path
 
-log_config = {
-    "version": 1,
-    "formatters": {
-        "file": {
-            "()": "uvicorn.logging.DefaultFormatter",
-            "fmt": "[%(levelname)s] %(asctime)s %(name)s:%(filename)s:%(lineno)d %(message)s",
-            "datefmt": "%Y-%m-%d %H:%M:%S",
-            "use_colors": None,
-        }
-    },
-    "handlers": {
-        "file": {
-            "formatter": "file",
-            "class": "logging.handlers.TimedRotatingFileHandler",
-            "filename": "logs/StreamDownloader.log",
-            "when": "D",  # Daily rotation
-            "interval": 1,  # Rotate once a day
-            "backupCount": 30,  # Keep last 7 days logs
-        }
-    },
-    "loggers": {
-        "uvicorn": {"handlers": ["file"], "level": "INFO", "propagate": False},
-        "uvicorn.error": {"level": "INFO"},
-        "uvicorn.access": {"handlers": ["file"], "level": "INFO", "propagate": False},
-    },
-}
-
 
 class logger:
     """
@@ -45,40 +18,36 @@ class logger:
 
     def __new__(self,
                 name,
-                level: int = 20,
-                log_filename: str = 'StreamDownloader.log',
+                level: int = 10 if os.getenv("DEBUG") else 20,
+                log_filename: str = None,
                 std_out: bool = True,
-                interval=log_config['handlers']['file']['interval'],
-                when=log_config['handlers']['file']['when'],
-                backupCount=log_config['handlers']['file']['backupCount'],
+                interval=1,
+                when="D",
+                backupCount=30,
                 **kwargs):
         lg = logging.getLogger(name)
         lg.setLevel(level)
         handlers = []
-        fmt = log_config["formatters"]['file']['fmt']
+        fmt = "[%(levelname)s] %(asctime)s %(filename)s:%(lineno)d %(message)s"
         formatter = logging.Formatter(fmt=fmt)
         if std_out:
             stream_handler = logging.StreamHandler(stream=sys.stdout)
             stream_handler.setFormatter(formatter)
             handlers.append(stream_handler)
-        # self.time_rotate_hanlder = None
+
         if log_filename:
             log_dir = Path(os.getenv("LOG_DIR") or "logs")
             log_dir.mkdir(exist_ok=True)
-            log_config['handlers']['file']['filename'] = log_dir.joinpath(
-                log_filename).as_posix()
 
-            self.time_rotate_hanlder = TimedRotatingFileHandler(
+            time_rotate_hanlder = TimedRotatingFileHandler(
                 # housekeeping is delete file by this filename as prefix
-                filename=log_config['handlers']['file']['filename'],
+                filename=log_dir.joinpath(log_filename),
                 interval=interval,
                 when=when,  # one file every {interval} Day
                 backupCount=backupCount,  # keep latest 30 files
-                encoding="utf-8",
-                delay=True)
-            self.time_rotate_hanlder.doRollover()
-            self.time_rotate_hanlder.setFormatter(formatter)
-            handlers.append(self.time_rotate_hanlder)
+                encoding="utf-8")
+            time_rotate_hanlder.setFormatter(formatter)
+            handlers.append(time_rotate_hanlder)
 
         for h in handlers:
             lg.addHandler(h)
--- send_video_to_tg_channel.py
+++ send_video_to_tg_channel.py
@@ -1,15 +1,17 @@
 import asyncio
 import os
+from asyncio import Semaphore
+from collections import deque
 from datetime import datetime
 from logging import Logger
 from pathlib import Path
-from typing import Optional
+from typing import Deque, Dict, Optional, Set
 
+import aiofiles
 from dotenv import load_dotenv
 from telegram import Bot, InputFile
 from telegram.error import NetworkError, RetryAfter, TimedOut
 from telegram.request import HTTPXRequest
-from tqdm import tqdm
 
 from handlers.mongo import MongoHandler
 from models.base import (BackupChannel, BackupInfoTimeStamp, BackupMessage,
@@ -18,11 +20,195 @@ from models.logger import logger
 from StreamBot.utils import error_msg
 
 
-async def send_mkv_video(bot_token: str, chat_id: str, video_path: str | Path, sn: str, log: Optional[Logger] = None):
+def init_mongo_handler():
+    h = MongoHandler()
+    h.default_col = 'AVB_VIDEOS'
+    h.default_sys_col = 'AVB_SYS'
+    return h
+
+
+def init_logger(name: str, log_filename: str, level: int = 10 if os.getenv("DEBUG") else 20):
+    return logger(name=name, log_filename=log_filename, level=level)
+
+
+async def download_file(file_path: Path, semaphore: Semaphore) -> Optional[Path]:
+    async with semaphore:
+        try:
+            async with aiofiles.open(file_path, 'rb') as file:
+                await file.read()  # 模擬文件讀取操作
+            return file_path
+        except Exception as e:
+            print(f"Error downloading {file_path}: {e}")
+            return None
+
+
+class DownloadManager:
+    def __init__(self, max_concurrent_downloads: int = 5):
+        self.download_queue: Deque[Path] = deque()
+        self.semaphore = Semaphore(max_concurrent_downloads)
+        self.downloaded_files: Dict[Path, Path] = {}
+        self.active_downloads: Set[asyncio.Task] = set()
+        self.total_preload: int = 0
+        self.completed_preload: int = 0
+
+    async def add_to_queue(self, file_path: Path):
+        self.download_queue.append(file_path)
+        self.total_preload += 1
+        task = asyncio.create_task(self._process_queue())
+        self.active_downloads.add(task)
+        task.add_done_callback(self.active_downloads.discard)
+
+    async def wait_all_downloads(self):
+        while self.active_downloads or self.download_queue:
+            await asyncio.sleep(0.1)
+
+    async def _process_queue(self):
+        while self.download_queue:
+            file_path = self.download_queue.popleft()
+            if file_path not in self.downloaded_files:
+                downloaded_file = await download_file(file_path, self.semaphore)
+                if downloaded_file:
+                    self.downloaded_files[file_path] = downloaded_file
+                    self.completed_preload += 1
+
+    async def get_downloaded_file(self, file_path: Path) -> Optional[Path]:
+        while file_path not in self.downloaded_files:
+            await asyncio.sleep(0.1)
+        return self.downloaded_files.get(file_path)
+
+    def get_preload_status(self):
+        return self.completed_preload, self.total_preload
+
+
+async def preload_videos(idx: int, docs: list[MongoDoc], dir_path: list[Path], log: Logger, download_manager: DownloadManager):
+    total_docs = len(docs)
+    preload_count = 0
+    print(f"預先下載影片")
+    for i in range(1, 11):  # 預先下載接下來的10個視頻
+        next_idx = idx + i
+        if next_idx < total_docs:
+            next_av_path = get_av_file(docs[next_idx], dir_path, log)
+            if next_av_path:
+                await download_manager.add_to_queue(next_av_path)
+                preload_count += 1
+    return preload_count
+
+
+async def main(log: Logger, h: MongoHandler):
+    download_manager = DownloadManager()
+
+    bot_token = os.getenv('TG_BOT_TOKEN')
+    chat_ids = os.getenv('TG_CHANNEL_IDS').split(',')
+    use_local_api = True
+    dir_path = [Path(p) for p in os.getenv('VID_DIR_PATH').split(',')]
+
+    results = h.aggregate([
+        {'$match': {'doc_type': 'av_info'}},
+        {'$sort': {'snap_date': 1}}], show_id=True)
+    docs = [MongoDoc.model_validate(doc)
+            for doc in results]
+
+    # 假設這是你的主要處理循環
+    for idx, doc in enumerate(docs):
+        av_path = get_av_file(doc, dir_path, log)
+        if av_path:
+            await download_manager.add_to_queue(av_path)
+
+        # 預先下載接下來的10個視頻
+        # 在後台啟動預加載任務
+        preload_task = asyncio.create_task(preload_videos(
+            idx, docs, dir_path, log, download_manager))
+
+        if av_path:
+            # 等待當前影片下載完成
+            downloaded_file = await download_manager.get_downloaded_file(av_path)
+            if downloaded_file:
+                # 使用下載好的文件進行後續操作
+                await process_video(h=h,
+                                    doc=doc,
+                                    file_path=downloaded_file,
+                                    bot_token=bot_token,
+                                    chat_ids=chat_ids,
+                                    log=log,
+                                    use_local_api=use_local_api)
+
+        # 等待預加載任務完成
+        preload_count = await preload_task
+
+        # 獲取預加載狀態
+        completed, total = download_manager.get_preload_status()
+
+        # # 檢查：在處理下一個視頻之前，確保至少有5個視頻在隊列中
+        # while len(download_manager.download_queue) < 5 and idx + 1 < len(docs):
+        #     completed, total = download_manager.get_preload_status()
+        #     log.info(
+        #         f"已處理 {idx + 1}/{len(docs)} 個視頻，"
+        #         f"預先載入: {preload_count} 個，"
+        #         f"載入完成: {completed}/{total}，"
+        #         f"當前下載隊列中有 {len(download_manager.download_queue)} 個視頻"
+        #     )
+        #     await asyncio.sleep(1)
+
+        # 添加一個進度日誌
+        log.info(
+            f"已處理 {idx + 1}/{len(docs)} 個視頻，"
+            f"預先載入: {preload_count} 個，"
+            f"載入完成: {completed}/{total}，"
+            f"當前下載隊列中有 {len(download_manager.download_queue)} 個視頻"
+        )
+
+    # 等待所有下載任務完成
+    await download_manager.wait_all_downloads()
+
+
+async def process_video(h: MongoHandler, doc: MongoDoc, file_path: Path, bot_token: str, chat_ids: list[str], log: Logger, use_local_api: bool = False):
+    # 這裡放置你的視頻處理和上傳邏輯
+    backup_channels = []
+    backup_messages = []
+    if doc.tg_backup:
+        for backup in doc.tg_backup:
+            backup_channels.append(BackupChannel(
+                **backup.model_dump(exclude=['message_timestamp', 'message_id'])))
+            backup_messages.append(BackupMessage(
+                **backup.model_dump(exclude=['message_timestamp'])))
+
+    title = file_path.absolute().parent.name[5:]
+
+    log.info(f"開始 Backup {title} to TG Group")
+    for chat_id in chat_ids:
+        bot_id = str(bot_token.split(':')[0])
+        backup_channel = BackupChannel(
+            bot_id=bot_id, chat_id=chat_id)
+        if backup_channel in backup_channels:
+            log.info(f'Skip at: {backup_channel}')
+            continue
+
+        async with aiofiles.open(file_path, 'rb') as file:
+            file_content = await file.read()
+            input_file = InputFile(file_content, filename=file_path.name)
+
+        backup_info_timestamp = await send_mkv_video(bot_token,
+                                                     chat_id,
+                                                     title=title,
+                                                     filename=file_path.name,
+                                                     input_file=input_file,
+                                                     sn=doc.SN,
+                                                     log=log,
+                                                     use_local_api=use_local_api)
+        if backup_info_timestamp is not None:
+            backup_info = BackupMessage(
+                **backup_info_timestamp.model_dump(exclude=['message_timestamp']))
+            if backup_info not in backup_messages:
+                log.info('更新 TG Backup 資訊')
+                h.update({'_id': doc.id},
+                         {'$push': {'tg_backup': backup_info_timestamp.model_dump()}})
+
+
+async def send_mkv_video(bot_token: str, chat_id: str, title: str, filename: str, input_file: InputFile, sn: str, log: Optional[Logger] = None, use_local_api: bool = False) -> Optional[BackupInfoTimeStamp]:
     if not log:
         log = print
     else:
-        log = log.debug
+        log = log.info
 
     bot_api_url = "https://api.telegram.org/bot"
     local_bot_api_url = 'http://localhost:8089/bot'
@@ -38,32 +224,23 @@ async def send_mkv_video(bot_token: str, chat_id: str, video_path: str | Path, s
               base_url=local_bot_api_url if use_local_api else bot_api_url)
     await bot.initialize()
 
-    if isinstance(video_path, str):
-        video_path = Path(video_path)
-    file_path = video_path.absolute()
-
     try:
-        title = file_path.parent.name[5:]
         log(f"上傳至 Channel: {chat_id} ...")
         st = datetime.now()
-        with open(file_path, 'rb') as fp:
-            fp.seek(0)
-            input_file = InputFile(fp.read(), filename=file_path.name)
-            message_timestamp = datetime.now()
-            video_message = await bot.send_video(
-                chat_id=chat_id,
-                video=input_file,
-                filename=file_path.name,
-                caption=f'番號: {sn or ""}\n片名: {title}\nBackup At: {message_timestamp.strftime("%Y-%m-%d")}',
-                parse_mode=None,
-                supports_streaming=True,
-            )
+        video_message = await bot.send_video(
+            chat_id=chat_id,
+            video=input_file,
+            filename=filename,
+            caption=f'番號: {sn or ""}\n片名: {title}\nBackup At: {st.strftime("%Y-%m-%d")}',
+            parse_mode=None,
+            supports_streaming=True,
+        )
         et = datetime.now()
         log(f"\n上傳成功！ 費時: {et - st}")
         backup_info = {'bot_id': str(bot.id),
                        'chat_id': str(chat_id),
                        'message_id': str(video_message.id),
-                       'message_timestamp': message_timestamp
+                       'message_timestamp': st
                        }
         return BackupInfoTimeStamp(**backup_info)
     except TimedOut:
@@ -77,76 +254,40 @@ async def send_mkv_video(bot_token: str, chat_id: str, video_path: str | Path, s
     except Exception as e:
         log(f"\n上傳視頻時發生錯誤: {type(e).__name__}: {e}")
 
+
+def get_av_file(doc: MongoDoc, dir_path: list[Path], log: Logger):
+    try:
+        video_dir_path = [d for d in dir_path if d.name == doc.parent]
+        if len(video_dir_path) > 0:
+            video_dir_path = video_dir_path[0]
+        if isinstance(doc.videos, list):
+            for video in doc.videos:
+                if video_dir_path and video.type == '.mkv':
+                    av_dir_path = video_dir_path.joinpath(doc.dir_name)
+                    if av_dir_path.exists():
+                        return av_dir_path.joinpath(f"{video.name}{video.type}")
+    except Exception as e:
+        log.error(f"get_av_file 發生錯誤：{error_msg(e)}")
+        return
+
+
 if __name__ == "__main__":
     load_dotenv()
-
     file_path = Path(__file__)
     os.environ['LOG_DIR'] = file_path.parent.joinpath('logs').as_posix()
-    log = logger(name=file_path.name,
-                 log_filename=f'TGBackup.log',
-                 level=10 if os.getenv("DEBUG") else 20)
-
-    bot_token = os.getenv('TG_BOT_TOKEN')
-    chat_ids = os.getenv('TG_CHANNEL_IDS').split(',')
-    use_local_api = True
+    log_config = {
+        'name': file_path.name,
+        'log_filename': 'TGBackup.log',
+    }
+    log = init_logger(**log_config)
+    h = init_mongo_handler()
 
     try:
-        h = MongoHandler()
-        h.default_col = 'AVB_VIDEOS'
-        h.default_sys_col = 'AVB_SYS'
-
-        dir_path = [Path(p) for p in os.getenv('VID_DIR_PATH').split(',')]
-
-        results = h.aggregate([
-            {'$match': {'doc_type': 'av_info'}},
-            {'$sort': {'snap_date': 1}}], show_id=True)
-        docs = [MongoDoc.model_validate(doc)
-                for doc in results]
-        for doc in tqdm(docs, desc="Backup Video to TG"):
-            video_dir_path = [d for d in dir_path if d.name == doc.parent]
-            if len(video_dir_path) > 0:
-                video_dir_path = video_dir_path[0]
-            if isinstance(doc.videos, list):
-                for video in doc.videos:
-                    if video.type == '.mkv':
-                        video_name = f"{video.name}{video.type}"
-                        if video_dir_path:
-                            av_dir_path = video_dir_path.joinpath(doc.dir_name)
-                            av_dir_exists = av_dir_path.exists()
-                            if av_dir_exists:
-                                av_path = av_dir_path.joinpath(video_name)
-
-                                backup_channels = []
-                                backup_messages = []
-                                if doc.tg_backup:
-                                    for backup in doc.tg_backup:
-                                        backup_channels.append(BackupChannel(
-                                            **backup.model_dump(exclude=['message_timestamp', 'message_id'])))
-                                        backup_messages.append(BackupMessage(
-                                            **backup.model_dump(exclude=['message_timestamp'])))
-
-                                title = av_path.absolute().parent.name[5:]
-                                log.debug(f"開始上傳 {title} ...")
-
-                                for chat_id in tqdm(chat_ids, desc=f"Backup {title} to TG Group", leave=False):
-                                    bot_id = str(bot_token.split(':')[0])
-                                    backup_channel = BackupChannel(
-                                        bot_id=bot_id, chat_id=chat_id)
-                                    if backup_channel in backup_channels:
-                                        log.debug(
-                                            f'Skip: {title} at: {backup_channel}')
-                                        continue
-
-                                    backup_info_timestamp = asyncio.run(
-                                        send_mkv_video(bot_token, chat_id, av_path, doc.SN, log))
-                                    if backup_info_timestamp is not None:
-                                        backup_info = BackupMessage(
-                                            **backup_info_timestamp.model_dump(exclude=['message_timestamp']))
-                                        if backup_info not in backup_messages:
-                                            log.debug('更新 TG Backup 資訊')
-                                            h.update({'_id': doc.id},
-                                                     {'$push': {'tg_backup': backup_info_timestamp.model_dump()}})
+        asyncio.run(main(log, h))
     except KeyboardInterrupt:
-        log.debug("\n用戶中斷了程序。")
+        log.info("\n用戶中斷了程序。")
     except Exception as e:
         log.error(f"發生錯誤：{error_msg(e)}")
+    finally:
+        h.client.close()
+        log.info("程序結束。")

b0cbf9c9d74448b3dc05b46882b6c82e2875bc3a
YueLi <green07111@gmail.com>
2024-11-08 13:38:20 +0800

    重構 ig.py，改進媒體資訊獲取邏輯，新增異常處理以增強穩定性

--- ig.py
+++ ig.py
@@ -19,6 +19,32 @@ if __name__ == '__main__':
     h.default_col = 'IGB_IMAGES'
     h.default_sys_col = 'IGB_SYS'
 
+    # Distinc Media Infos
+    # h.aggregate([
+    #     # 可選：先按某個字段排序
+    #     # 按照快照日期降序
+    #     {
+    #         '$sort': {
+    #             'snap_date': -1
+    #         }
+    #     },
+    #     {
+    #         '$group': {
+    #             '_id': {
+    #                 'dir_name': "$dir_name",
+    #                 'name': "$name",
+    #                 'type': "$type"
+    #             },
+    #             'original_doc': {'$first': "$$ROOT"}
+    #         }
+    #     },
+    #     {
+    #         '$replaceRoot': {
+    #             'newRoot': "$original_doc"
+    #         }
+    #     }
+    # ])
+
     dir_path = [Path(p) for p in os.getenv('IG_DIR_PATH').split(',')]
     ig_users: List[Path] = []
     for dp in dir_path:
@@ -30,121 +56,135 @@ if __name__ == '__main__':
     media_types = ['*']
     desc = 'IG User Progress'
     progress = tqdm(ig_users, desc=desc)
+    finished = False
     for p in progress:
-        update = False
-        dir_name = p.name
-        progress.set_description(f'{desc} {dir_name}')
-        parent = p.parent.name
+        try:
+            update = False
+            dir_name = p.name
+            progress.set_description(f'{desc} {dir_name}')
+            parent = p.parent.name
 
-        name_split = p.name.split('_')
-        src = name_split[0]
-        title = '_'.join(name_split[1:])
+            name_split = p.name.split('_')
+            src = name_split[0]
+            title = '_'.join(name_split[1:])
 
-        for t in media_types:
-            fs = list(p.glob(f'*.{t}'))
-            for f in tqdm(fs, desc='IG User Medias', leave=False):
-                fname = f.stem
-                ftype = f.suffix
-                if ftype.lower() not in ['.jpg', '.jpeg', '.png', '.mp4']:
-                    continue
+            for t in media_types:
+                fs = list(p.glob(f'*.{t}'))
+                for f in tqdm(fs, desc='IG User Medias', leave=False):
+                    fname = f.stem
+                    ftype = f.suffix
+                    if ftype.lower() not in ['.jpg', '.jpeg', '.png', '.mp4']:
+                        continue
 
-                # Check Exists
-                doc = h.query_one({'dir_name': dir_name,
-                                   'parent': parent,
-                                   'title': title,
-                                   'name': fname,
-                                   'type': ftype}) or {}
-                doc_width = doc.get('width')
-                doc_height = doc.get('height')
-                doc_size = doc.get('size')
+                    # Check Exists
+                    doc = h.query_one({'dir_name': dir_name,
+                                       'parent': parent,
+                                       'title': title,
+                                       'name': fname,
+                                       'type': ftype},
+                                      show_id=True) or {}
+                    doc_width = doc.get('width')
+                    doc_height = doc.get('height')
+                    doc_size = doc.get('size')
 
-                old_docs = list(h.query({'dir_name': dir_name,
-                                         'title': title,
-                                         'name': fname,
-                                         'type': ftype})) or []
-                if len(old_docs) == 1:
-                    old_doc = old_docs[0]
-                else:
-                    old_doc = {}
-                old_doc_width = old_doc.get('width')
-                old_doc_height = old_doc.get('height')
-                old_doc_size = old_doc.get('size')
+                    old_docs = list(h.query({'dir_name': dir_name,
+                                            'title': title,
+                                             'name': fname,
+                                             'type': ftype},
+                                            show_id=True)) or []
+                    if len(old_docs) == 1:
+                        old_doc = old_docs[0]
+                    else:
+                        old_doc = {}
+                    old_doc_width = old_doc.get('width')
+                    old_doc_height = old_doc.get('height')
+                    old_doc_size = old_doc.get('size')
 
-                if not doc_width is None and not doc_height is None and not doc_size is None:
-                    media_infos = {'width': doc_width,
-                                   'height': doc_height,
-                                   'size': doc_size}
-                elif not old_doc_width is None and not old_doc_height is None and not old_doc_size is None:
-                    media_infos = {'width': old_doc_width,
-                                   'height': old_doc_height,
-                                   'size': old_doc_size}
-                else:
-                    media_infos = {}
+                    if not doc_width is None and not doc_height is None and not doc_size is None:
+                        media_infos = {'width': doc_width,
+                                       'height': doc_height,
+                                       'size': doc_size}
+                    elif not old_doc_width is None and not old_doc_height is None and not old_doc_size is None:
+                        media_infos = {'width': old_doc_width,
+                                       'height': old_doc_height,
+                                       'size': old_doc_size}
+                    else:
+                        # Empty media_infos
+                        log.info('New Document')
+                        media_infos = {'width': 0, 'height': 0}
 
-                if len(media_infos) == 0 or not media_infos.get('width') or not media_infos.get('height'):
-                    fp = f.as_posix()
-                    try:
-                        if ftype in ['.mp4']:
-                            media_infos = get_video_infos(fp)
-                        else:
-                            media_infos = get_image_infos(fp)
-                            if not media_infos.get('width') or not media_infos.get('height'):
-                                # Empty file path, try use PIL ...
-                                media_infos = get_image_infos_pil(fp)
-                        if not media_infos.get('width') or not media_infos.get('height'):
-                            os.remove(fp)
-                            continue
-                    except Exception as err:
-                        log.info(err)
-                        sys.exit(1)
-                    media_infos['size'] = os.path.getsize(fp)
+                    if media_infos['width'] == 0 or media_infos['height'] == 0:
+                        # Get Media Infos
+                        log.info(f'Get Media Infos: {f}')
+                        fp = f.as_posix()
+                        try:
+                            if ftype in ['.mp4']:
+                                media_infos = get_video_infos(fp)
+                            else:
+                                media_infos = get_image_infos(fp)
+                                if media_infos['width'] == 0 or media_infos['height'] == 0:
+                                    # Empty file path, try use PIL ...
+                                    media_infos = get_image_infos_pil(fp)
+                            if media_infos['width'] == 0 or media_infos['height'] == 0:
+                                os.remove(fp)
+                                continue
+                        except Exception as err:
+                            log.info(err)
+                            sys.exit(1)
+                        media_infos['size'] = os.path.getsize(fp)
 
-                # To Document Obj
-                if doc.get('dir_name') != dir_name:
-                    doc['dir_name'] = dir_name
-                    update = True
+                    # To Document Obj
+                    if doc.get('dir_name') != dir_name:
+                        doc['dir_name'] = dir_name
+                        update = True
 
-                if doc.get('parent') != parent:
-                    doc['parent'] = parent
-                    update = True
+                    if doc.get('parent') != parent:
+                        doc['parent'] = parent
+                        update = True
 
-                if doc.get('source') != src:
-                    doc['source'] = src
-                    update = True
+                    if doc.get('source') != src:
+                        doc['source'] = src
+                        update = True
 
-                old_title = doc.get('title')
-                if old_title != title:
-                    doc['title'] = title
-                    update = True
+                    old_title = doc.get('title')
+                    if old_title != title:
+                        doc['title'] = title
+                        update = True
 
-                old_name = doc.get('name')
-                if old_name != fname:
-                    doc['name'] = fname
-                    update = True
+                    old_name = doc.get('name')
+                    if old_name != fname:
+                        doc['name'] = fname
+                        update = True
 
-                old_type = doc.get('type')
-                if old_type != ftype:
-                    doc['type'] = ftype
-                    update = True
+                    old_type = doc.get('type')
+                    if old_type != ftype:
+                        doc['type'] = ftype
+                        update = True
 
-                old_width = doc_width
-                if old_width != media_infos.get('width'):
-                    doc['width'] = media_infos.get('width')
-                    update = True
+                    old_width = doc_width
+                    if old_width != media_infos.get('width'):
+                        doc['width'] = media_infos.get('width')
+                        update = True
 
-                old_height = doc_height
-                if old_height != media_infos.get('height'):
-                    doc['height'] = media_infos.get('height')
-                    update = True
+                    old_height = doc_height
+                    if old_height != media_infos.get('height'):
+                        doc['height'] = media_infos.get('height')
+                        update = True
 
-                old_size = doc_size
-                if old_size != media_infos.get('size'):
-                    doc['size'] = media_infos.get('size')
-                    update = True
+                    old_size = doc_size
+                    if old_size != media_infos.get('size'):
+                        doc['size'] = media_infos.get('size')
+                        update = True
 
-                if update:
-                    doc['snap_date'] = snap_date
-                    docs.append(doc)
+                    if update:
+                        doc['snap_date'] = snap_date
+                        docs.append(doc)
+        except KeyboardInterrupt:
+            log.info('KeyboardInterrupt')
+            log.info(f'Docs Size: {len(docs)}')
+            finished = True
+        if finished:
+            break
 
     if docs:
         new_docs = []
